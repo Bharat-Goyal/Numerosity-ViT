9457f49877d5414c0b48ca0c2ba8e69e6176a75b
Bharat Goyal <2002bharatgoyal@gmail.com>
2023-12-08 18:09:01 -0500

    fixed area bug in generation, cleanup

--- utils/generate_dataset.py
+++ utils/generate_dataset.py
@@ -30,41 +30,37 @@ def generate_exp2_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_d
                 cv2.imwrite(filepath, image)
                 ct += 1
 
+def generateShape(shape, area_per_shape, image): 
+    if shape == 'circle':
+        radius = int(np.sqrt(area_per_shape / np.pi))
+        x = random.randint(radius, 720 - radius)
+        y = random.randint(radius, 720 - radius)
+        cv2.circle(image, (x + radius, y + radius), radius, 0, -1)
+    elif shape == 'square':
+        side_length = int(np.sqrt(area_per_shape))
+        x = random.randint(0, 720 - side_length)
+        y = random.randint(0, 720 - side_length)
+        cv2.rectangle(image, (x, y), (x + side_length, y + side_length), 0, -1)
+    elif shape == 'triangle':
+        side_length = int(np.sqrt(2*area_per_shape))
+        x = random.randint(0, 720 - side_length)
+        y = random.randint(0, 720 - side_length)
+        points = np.array([[x, y], [x + side_length, y], [x + side_length // 2, y + side_length]])
+        cv2.fillPoly(image, [points], 0)
 
-          
 def generate_exp3_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_data/exp3_equal_area_diff_shapes'):
     shapes = ['circle', 'square', 'triangle']
-    areas = [0.02, 0.04, 0.06, 0.08, 0.1]  # Percentage of image area
+    areas = [0.0002, 0.0004, 0.0006, 0.0008, 0.001]  # Percentage of image area
 
     for i in range(1, 10):
         ct = 1
         for area_percentage in areas:
             for _ in range(4):
                 image = np.ones((720, 720), dtype=np.uint8) * 255
-                cumulative_area = 0
                 shape = random.choice(shapes)
                 area_per_shape = (area_percentage * 720 * 720) / i
                 for _ in range(i):
-                    if shape == 'circle':
-                        radius = int(np.sqrt(area_per_shape / np.pi))
-                        x = random.randint(radius, 720 - radius)
-                        y = random.randint(radius, 720 - radius)
-                        cv2.circle(image, (x + radius, y + radius), radius, 0, -1)
-                        cumulative_area += np.pi * radius * radius
-                    elif shape == 'square':
-                        side_length = int(np.sqrt(area_per_shape))
-                        x = random.randint(0, 720 - side_length)
-                        y = random.randint(0, 720 - side_length)
-                        cv2.rectangle(image, (x, y), (x + side_length, y + side_length), 0, -1)
-                        cumulative_area += side_length * side_length
-                    elif shape == 'triangle':
-                        side_length = int(np.sqrt(2*area_per_shape))
-                        x = random.randint(0, 720 - side_length)
-                        y = random.randint(0, 720 - side_length)
-                        points = np.array([[x, y], [x + side_length, y], [x + side_length // 2, y + side_length]])
-                        cv2.fillPoly(image, [points], 0)
-                        cumulative_area += 0.5 * side_length * side_length
-
+                    generateShape(shape, area_per_shape, image)
                 filename = f"{i}_{ct}.png"
                 loc = f"{directory}/{i}"
                 if not os.path.exists(loc):
@@ -76,7 +72,7 @@ def generate_exp3_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_d
           
 def generate_exp4_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_data/exp4_diff_area_diff_shapes'):
     shapes = ['circle', 'square', 'triangle']
-    areas = [0.02, 0.04, 0.06, 0.08, 0.1]  # Percentage of image area
+    areas = [0.0002, 0.0004, 0.0006, 0.0008, 0.001]  # Percentage of image area
 
     for i in range(1, 10):
         ct = 1
@@ -88,26 +84,7 @@ def generate_exp4_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_d
                 shape = random.choice(shapes)
                 area_per_shape = (area_percentage * 720 * 720) / i
                 for _ in range(i):
-                    if shape == 'circle':
-                        radius = int(np.sqrt(area_per_shape / np.pi))
-                        x = random.randint(radius, 720 - radius)
-                        y = random.randint(radius, 720 - radius)
-                        cv2.circle(image, (x + radius, y + radius), radius, 0, -1)
-                        cumulative_area += np.pi * radius * radius
-                    elif shape == 'square':
-                        side_length = int(np.sqrt(area_per_shape))
-                        x = random.randint(0, 720 - side_length)
-                        y = random.randint(0, 720 - side_length)
-                        cv2.rectangle(image, (x, y), (x + side_length, y + side_length), 0, -1)
-                        cumulative_area += side_length * side_length
-                    elif shape == 'triangle':
-                        side_length = int(np.sqrt(2*area_per_shape))
-                        x = random.randint(0, 720 - side_length)
-                        y = random.randint(0, 720 - side_length)
-                        points = np.array([[x, y], [x + side_length, y], [x + side_length // 2, y + side_length]])
-                        cv2.fillPoly(image, [points], 0)
-                        cumulative_area += 0.5 * side_length * side_length
-
+                    generateShape(shape, area_per_shape, image)
                 filename = f"{i}_{ct}.png"
                 loc = f"{directory}/{i}"
                 if not os.path.exists(loc):
@@ -118,7 +95,7 @@ def generate_exp4_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_d
     
 def generate_exp5_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_data/exp5_diff_area_diff_shapes_in_img'):
     shapes = ['circle', 'square', 'triangle']
-    areas = [0.02, 0.04, 0.06, 0.08, 0.1]  # Percentage of image area
+    areas = [0.0002, 0.0004, 0.0006, 0.0008, 0.001]  # Percentage of image area
 
     for i in range(1, 10):
         ct = 1
@@ -126,30 +103,10 @@ def generate_exp5_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_d
             area_percentage = random.choice(areas)
             for _ in range(4):
                 image = np.ones((720, 720), dtype=np.uint8) * 255
-                cumulative_area = 0
                 area_per_shape = (area_percentage * 720 * 720) / i
                 for _ in range(i):
                     shape = random.choice(shapes)
-                    if shape == 'circle':
-                        radius = int(np.sqrt(area_per_shape / np.pi))
-                        x = random.randint(radius, 720 - radius)
-                        y = random.randint(radius, 720 - radius)
-                        cv2.circle(image, (x + radius, y + radius), radius, 0, -1)
-                        cumulative_area += np.pi * radius * radius
-                    elif shape == 'square':
-                        side_length = int(np.sqrt(area_per_shape))
-                        x = random.randint(0, 720 - side_length)
-                        y = random.randint(0, 720 - side_length)
-                        cv2.rectangle(image, (x, y), (x + side_length, y + side_length), 0, -1)
-                        cumulative_area += side_length * side_length
-                    elif shape == 'triangle':
-                        side_length = int(np.sqrt(2*area_per_shape))
-                        x = random.randint(0, 720 - side_length)
-                        y = random.randint(0, 720 - side_length)
-                        points = np.array([[x, y], [x + side_length, y], [x + side_length // 2, y + side_length]])
-                        cv2.fillPoly(image, [points], 0)
-                        cumulative_area += 0.5 * side_length * side_length
-
+                    generateShape(shape, area_per_shape, image)
                 filename = f"{i}_{ct}.png"
                 loc = f"{directory}/{i}"
                 if not os.path.exists(loc):
@@ -159,6 +116,6 @@ def generate_exp5_images(directory='/home/hice1/bgoyal7/scratch/HML/experiment_d
                 ct += 1
     
 if __name__ == '__main__': 
-    # generate_exp2_images()
+    # generate_exp3_images()
     generate_exp4_images()
     generate_exp5_images()
